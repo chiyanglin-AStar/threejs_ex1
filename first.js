54d532e6ae2dd298c100670c78146dc4e76860d7
chiyanglin <chiyanglin@gmail.com>
2021-10-10 19:59:23 +0000

    update sync command

--- first.js
+++ first.js
@@ -1,7 +1,7 @@
 var http = require('http');
-console.log("this server is runnning on http://localhost:3000 ");
+console.log("this server is runnning fff on http://localhost:4000 ");
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
   //console.log("this server is runnning on localhost:8080 ");
-}).listen(3000);
+}).listen(4000);
