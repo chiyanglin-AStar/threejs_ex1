6bb16ebf4093f5905049258f7d8aed65d9632293
CodingCoffee <88169585+CodingCoffee-01@users.noreply.github.com>
2022-02-18 14:38:23 +0800

    Delete first.js

--- first.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-console.log("this server is runnning fff on http://localhost:4000 ");
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-  //console.log("this server is runnning on localhost:8080 ");
-}).listen(4000);
